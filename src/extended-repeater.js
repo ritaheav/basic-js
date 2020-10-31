13abf95350f16141fd600462a7ea135315d3c48a
Ritaheav <rita20071995@hotmail.com>
2020-10-31 12:45:47 +0300

    add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result = '';
+    const repeatTimes = options.repeatTimes || 1;
+    const separator = options.separator || '+';
+    let addition = typeof options.addition === 'boolean' || options.addition === null ? String(options.addition) : options.addition || '';   
+    const additionRepeatTimes = options.additionRepeatTimes || 1;
+    const additionSeparator = options.additionSeparator || '|';
+    let add = addition;
+    if (additionRepeatTimes > 1) {
+        add = additionSeparator + addition;
+    }
+    const pattern = str + addition + add.repeat(additionRepeatTimes - 1);
+    for (let i = 1; i <= repeatTimes; i++) {
+        result += pattern;
+        if (i < repeatTimes) {
+            result += separator;
+        }
+    }
+    return result;
 };
   
